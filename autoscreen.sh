567b9bafb0e2acb6fa25d270d236b74f7642ca0a
solar_coder <souradeep.bond.007@gmail.com>
2023-03-11 13:04:53 +0530

    Update autoscreen.sh

--- autoscreen.sh
+++ autoscreen.sh
@@ -1,39 +1,40 @@
-function settile() {
-    #for zsh
-    echo -ne "\e]1;$@\a"
-    # for bash echo -ne '\033]0;'"$1"'\a'
-    # for windows terminal with bash
-    #export PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
-    #echo -ne '\033]0;'"$1"'\a'
-}
-}
-function passwordless_proxy() {
-    ssh-copy-id -o ProxyJump=$1 $2
-}
-
-function passwordless() {
-    ssh-copy-id  $@
-}
-
-function autoscreen() {
-  s=`ssh -t $@ 'screen -ls'`
-  list=`echo $s | grep Detached` > /dev/null 2>&1
-  if [[ $? -eq 0 ]]
-  then
-      echo $list | cut -d "." -f1
-      echo 'choose session'
-      read session_name
-   fi
-  if test -z "$session_name"
-  then
-      session_name=autosession-'$RANDOM$RANDOM$RANDOM$RANDOM'
-  fi
-  touch $HOME/$session_name.$$
-  settile $session_name$@
-  AUTOSSH_GATETIME=5 autossh -M 0 -- -o "ServerAliveInterval 5" -o "ServerAliveCountMax 1" -t $@ $'bash -c \'tmpScreenConfig=$(mktemp); echo "termcapinfo xterm* ti@:te" >> $tmpScreenConfig; echo "altscreen on" >> $tmpScreenConfig; echo "maptimeout 0" >> $tmpScreenConfig; echo "startup_message off" >> $tmpScreenConfig; echo "msgwait 0" >> $tmpScreenConfig; exec screen -c $tmpScreenConfig -S "'$session_name$'" -RD\'';
-  echo "session id is"
-  echo $session_name
-  echo "if possible save the current terminal log to a file"
-}
-#put it in .zshrc or .bashrc
-
+  6 function settile() {
+  7     echo -ne "\e]1;$@\a"
+  8 }
+  9 function passwordless_proxy() {
+ 10     ssh-copy-id -o ProxyJump=$1 $2
+ 11 }
+ 12
+ 13 function passwordless() {
+ 14     ssh-copy-id  $@
+ 15 }
+ 16 function autoscreen() {
+ 17   s=`ssh -t $@ 'screen -ls'`
+ 18   reattach=0
+ 19   list="`echo $s | grep Detached`" > /dev/null 2>&1
+ 20   if [[ "$?" -eq 0 ]]
+ 21   then
+ 22       echo $list | cut -d "." -f1
+ 23       echo 'choose session'
+ 24       read session_name
+ 25   fi
+ 26   if test -z "$session_name"
+ 27   then
+ 28       session_name=autosession-'$RANDOM$RANDOM$RANDOM$RANDOM'
+ 29   else
+ 30       reattach=1
+ 31   fi
+ 32   settile $session_name-$@
+ 33   AUTOSSH_GATETIME=5 autossh -M 0 -- -o "ServerAliveInterval 5" -o "ServerAliveCountMax 1" -t $@ $'bash -c \'tmpScreenConfig=$(mktemp); echo "termcapinfo xterm* ti@:te" >> $tmpScreenConfig; echo "altscreen
+     on" >> $tmpScreenConfig; echo "maptimeout 0" >> $tmpScreenConfig; echo "startup_message off" >> $tmpScreenConfig; echo "msgwait 0" >> $tmpScreenConfig; exec screen -c $tmpScreenConfig -S "'$session_name$'
+    " -RD\'';
+ 34   echo "session id is"
+ 35   echo $session_name
+ 36   dir=/mnt/c/Users/schakrabarti/"$session_name"/"$(date +"%d-%m-%Y")"
+ 37   echo $dir
+ 38   if [[ ! -d "$dir" ]]
+ 39   then
+ 40     mkdir -p $dir
+ 41   fi
+ 42   echo "if possible save the current terminal log to a file"
+ 43 }
